0f4446d9e713456bfe9d636ee4e3048d82075452
William Wang <william.wang650@gmail.com>
2020-02-26 20:54:18 -0800

    test

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,13 +6,13 @@
 /*   By: wiwang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/22 16:15:52 by wiwang            #+#    #+#             */
-/*   Updated: 2020/02/26 20:32:55 by wiwang           ###   ########.fr       */
+/*   Updated: 2020/02/26 20:53:14 by wiwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static	int make_num(char *s, int i, int num, char sign)
+static	int make_num(char *s, int i, long num, char sign)
 {
 	if (!((s[i] == '-') || (s[i] == '+')) &&
 		!((s[i + 1] >= '0') && (s[i + 1] <= '9')))
@@ -21,18 +21,21 @@ static	int make_num(char *s, int i, int num, char sign)
 		sign = s[i++];
 	while (s[i] >= '0' && s[i] <= '9')
 		num = (num * 10) + (s[i++] - '0');
+	if (num < 0 && sign == '\0')
+		return (-1);
+	if (num < 0 && sign == '-')
+		return (0);
 	if (sign == '-')
 		return (num * -1);
 	else
 		return (num);
-
 }
 
 int ft_atoi(const char *str)
 {
 	char	*s;
 	int		i;
-	int		num;
+	long	 num;
 	char	sign;
 
 	s = (char *)str;
@@ -44,5 +47,5 @@ int ft_atoi(const char *str)
 		if (s[i++] == '\e')
 			return (0);
 	}
-		return (make_num(s, i, num, sign));
+	return (make_num(s, i, num, sign));
 }
