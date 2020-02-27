e2528bf354967719960887c676137909e27d2b0d
William Wang <william.wang650@gmail.com>
2020-02-26 19:02:19 -0800

    test

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: wiwang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/20 14:07:46 by wiwang            #+#    #+#             */
-/*   Updated: 2020/02/26 18:36:05 by wiwang           ###   ########.fr       */
+/*   Updated: 2020/02/26 19:01:48 by wiwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
 	unsigned char		*target;
-	const unsigned char	*copy;
+	unsigned char		*copy;
 	size_t				i;
 	int					*nullptr;
 
 	target = (unsigned char *)dst;
-	copy = (const unsigned char *)src;
+	copy = (unsigned char *)src;
 	i = 0;
 	nullptr = NULL;
 	while (i < n)
