bdfd361f4a23fc035c90e33a3effd46af4a91457
William Wang <william.wang650@gmail.com>
2020-02-26 18:30:23 -0800

    strsub fix

--- ft_strsub.c
+++ ft_strsub.c
@@ -6,7 +6,7 @@
 /*   By: wiwang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/24 16:14:43 by wiwang            #+#    #+#             */
-/*   Updated: 2020/02/24 16:30:10 by wiwang           ###   ########.fr       */
+/*   Updated: 2020/02/26 18:29:50 by wiwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,14 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len)
 
 	sub = (char *)malloc(ft_strlen(s) * sizeof(*s));
 	i = 0;
+	if (sub == NULL)
+		return (NULL);
 	while (i < len)
 	{
 		sub[i] = s[start];
 		i++;
 		start++;
 	}
+	sub[i] = '\0';
 	return (sub);
 }
